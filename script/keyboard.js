a21df2d7b5303d3d2c8f392eca23034835a7bf03
Agu-F26 <agustinfacal96@gmail.com>
2022-04-17 05:05:55 -0300

    va queriendo

--- script/keyboard.js
+++ script/keyboard.js
@@ -1,5 +1,9 @@
-function hacerCosas(){
-    console.log("paso algo");
+function noTengoIdea(value){
+    document.getElementById("output").innerHTML = value;
+    console.log(value);
 }
+
+//document.getElementById("0").onclick = noTengoIdea();
+
 
 console.log("funciona");
